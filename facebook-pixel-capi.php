53bd6890864d1c998a83bac6839808f4e004d863
mdshukurmiah <miah5674@gmail.com>
2026-04-06 14:23:31 +0600

    Bump version to 1.0.1 in plugin file and README.md

--- facebook-pixel-capi.php
+++ facebook-pixel-capi.php
@@ -3,7 +3,7 @@
  * Plugin Name: W3 Pixel Server-Side Tracking
  * Plugin URI: https://github.com/mdshukurmiah/w3-pixel-capi
  * Description: A WordPress plugin that enables Facebook Pixel server-side tracking using the Conversions API (CAPI) for improved tracking accuracy and reliability.
- * Version: 1.0.0
+ * Version: 1.0.1
  * Author: Md Shukur Miah
  * Author URI: https://www.shukurs.com/
  * License: GPL v2 or later
